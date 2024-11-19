bb6f561b976c8139276455d1817884263391af61
Kelly <steffinthesky@gmail.com>
2024-11-19 04:04:38 +0000

    Ajustando o código pra deixar modular

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,43 +1,67 @@
-#include <stdio.h>
+/*
+    Adicionei funções pra facilitar minha vida na hora de cadastrar mais cidades.
+    Pra usar as funções, fiz uma estrutura e tive que trocar pra usar ponteiros e o valor se manter entre elas.
+*/
 
-int main() {
-    char estado, codigoCarta[4], nomeCidade[50];
+#include <stdio.h>
+typedef struct {
+    char estado, codigo[4], nome[50];
     int populacao, pontosTuristicos;
     float area, pib, densidadePopulacional, pibPerCapita;
+} Cidade;
 
+void lerDados(Cidade* cidade);
+void calcularPropriedades(Cidade* cidade);
+void printarDados(Cidade* cidade);
+
+int main() {
+    Cidade* cidade = malloc(sizeof(Cidade));
+
+    lerDados(cidade);
+    calcularPropriedades(cidade);
+    printarDados(cidade);
+
+    free(cidade);
+    return 0;
+}
+
+void lerDados(Cidade* cidade) {
     printf("Digite o estado: ");
-    scanf("%c", &estado);
+    scanf("%c", &cidade->estado);
 
     printf("Digite o Código da Carta: ");
-    scanf("%s", codigoCarta);
+    scanf("%s", cidade->codigo);
 
     printf("Digite o Nome da Cidade: ");
-    scanf(" %[^\n]s", nomeCidade);
+    scanf(" %[^\n]s", cidade->nome);
 
     printf("Digite a População: ");
-    scanf("%d", &populacao);
+    scanf("%d", &cidade->populacao);
 
     printf("Digite a Área: ");
-    scanf("%f", &area);
+    scanf("%f", &cidade->area);
 
     printf("Digite o PIB: ");
-    scanf("%f", &pib);
+    scanf("%f", &cidade->pib);
 
     printf("Digite a Número de Pontos Turísticos: ");
-    scanf("%d", &pontosTuristicos);
+    scanf("%d", &cidade->pontosTuristicos);
+}
 
-    densidadePopulacional = (float)populacao/area;
-    pibPerCapita = (pib/(float)populacao)*1000000000;
+void calcularPropriedades(Cidade* cidade) {
+    int bilhao = 1000000000;
+    cidade->densidadePopulacional = (float)cidade->populacao/cidade->area;
+    cidade->pibPerCapita = (cidade->pib/(float)cidade->populacao)*bilhao;
+}
 
-    printf("\nEstado: %c\n", estado); 
-    printf("Código da Carta: %s\n", codigoCarta); 
-    printf("Nome da Cidade: %s\n", nomeCidade); 
-    printf("População: %d\n", populacao); 
-    printf("Área: %.2f km²\n", area); 
-    printf("Densidade Populacional: %.2f pessoas/km²\n", densidadePopulacional); 
-    printf("PIB: %.2f bilhões de reais\n", pib); 
-    printf("PIB per Capita: %.2f reais\n", pibPerCapita); 
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos); 
-
-    return 0;
+void printarDados(Cidade* cidade) {
+    printf("\nEstado: %c\n", cidade->estado); 
+    printf("Código da Carta: %s\n", cidade->codigo); 
+    printf("Nome da Cidade: %s\n", cidade->nome); 
+    printf("População: %d\n", cidade->populacao); 
+    printf("Área: %.2f km²\n", cidade->area); 
+    printf("Densidade Populacional: %.2f pessoas/km²\n", cidade->densidadePopulacional); 
+    printf("PIB: %.2f bilhões de reais\n", cidade->pib); 
+    printf("PIB per Capita: %.2f reais\n", cidade->pibPerCapita); 
+    printf("Número de Pontos Turísticos: %d\n", cidade->pontosTuristicos); 
 }
