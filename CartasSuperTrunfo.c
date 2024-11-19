00dc5b5a6b69fb3510fb845b2ca49c84a79bb0f7
Kelly <steffinthesky@gmail.com>
2024-11-19 03:47:37 +0000

    Nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
     char estado, codigoCarta[4], nomeCidade[50];
     int populacao, pontosTuristicos;
-    float area, pib;
+    float area, pib, densidadePopulacional, pibPerCapita;
 
     printf("Digite o estado: ");
     scanf("%c", &estado);
@@ -27,12 +26,17 @@ int main() {
     printf("Digite a Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos);
 
+    densidadePopulacional = (float)populacao/area;
+    pibPerCapita = (pib/(float)populacao)*1000000000;
+
     printf("\nEstado: %c\n", estado); 
     printf("Código da Carta: %s\n", codigoCarta); 
     printf("Nome da Cidade: %s\n", nomeCidade); 
     printf("População: %d\n", populacao); 
-    printf("Área: %f km²\n", area); 
-    printf("PIB: %f bilhões de reais\n", pib); 
+    printf("Área: %.2f km²\n", area); 
+    printf("Densidade Populacional: %.2f pessoas/km²\n", densidadePopulacional); 
+    printf("PIB: %.2f bilhões de reais\n", pib); 
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita); 
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos); 
 
     return 0;
