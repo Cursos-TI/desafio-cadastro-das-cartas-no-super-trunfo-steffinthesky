b6435d788508552fd54051140b50fd7be4574e75
Kelly <steffinthesky@gmail.com>
2024-11-19 03:28:55 +0000

    Nível novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
+#include <string.h>
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado, codigoCarta[4], nomeCidade[50];
+    int populacao, pontosTuristicos;
+    float area, pib;
+
+    printf("Digite o estado: ");
+    scanf("%c", &estado);
+
+    printf("Digite o Código da Carta: ");
+    scanf("%s", codigoCarta);
+
+    printf("Digite o Nome da Cidade: ");
+    scanf(" %[^\n]s", nomeCidade);
+
+    printf("Digite a População: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a Área: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
+
+    printf("Digite a Número de Pontos Turísticos: ");
+    scanf("%d", &pontosTuristicos);
+
+    printf("\nEstado: %c\n", estado); 
+    printf("Código da Carta: %s\n", codigoCarta); 
+    printf("Nome da Cidade: %s\n", nomeCidade); 
+    printf("População: %d\n", populacao); 
+    printf("Área: %f km²\n", area); 
+    printf("PIB: %f bilhões de reais\n", pib); 
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos); 
 
     return 0;
 }
